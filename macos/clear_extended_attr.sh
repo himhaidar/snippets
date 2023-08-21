6a281171bdae70a7fd55a0dc95e6fedf940fd212
Haidar Ali <himhaidar@icloud.com>
2023-08-21 18:15:54 +0500

    add: clear_extended_attr.sh

---
+++ macos/clear_extended_attr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <file1> <file2> ..."
+    exit 1
+fi
+
+for arg in "$@"; do
+    if [ -f "$arg" ]; then
+        sudo xattr -cr "$arg"
+    fi
+done
+
